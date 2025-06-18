7ec015963ea51ed8eb34637556cea72d9d8ffda9
snennis <ddennisddv@gmail.com>
2025-06-18 10:34:54 +0200

    add spanish es.ts without translation of success_links and change Sevre to server

---
+++ i18n/es.ts
@@ -0,0 +1,338 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="es_ES" sourcelanguage="en_US">
+<context>
+    <name>MainDialog</name>
+    <message>
+        <location filename="../main_dialog.py" line="102"/>
+        <source>Name</source>
+        <translation>Nombre</translation>
+    </message>
+    <message>
+        <location filename="../main_dialog.py" line="103"/>
+        <source>Mapbender URL</source>
+        <translation>URL de Mapbender</translation>
+    </message>
+    <message>
+        <location filename="../main_dialog.py" line="110"/>
+        <source>Add server configuration</source>
+        <translation>Añadir configuración del servidor</translation>
+    </message>
+    <message>
+        <location filename="../main_dialog.py" line="111"/>
+        <source>Duplicate selected server configuration</source>
+        <translation>Duplicar la configuración del servidor seleccionado</translation>
+    </message>
+    <message>
+        <location filename="../main_dialog.py" line="112"/>
+        <source>Edit selected server configuration</source>
+        <translation>Editar la configuración del servidor seleccionado</translation>
+    </message>
+    <message>
+        <location filename="../main_dialog.py" line="113"/>
+        <source>Remove selected server configuration</source>
+        <translation>Eliminar la configuración del servidor seleccionado</translation>
+    </message>
+    <message>
+        <location filename="../main_dialog.py" line="143"/>
+        <source>Close</source>
+        <translation>Cerrar</translation>
+    </message>
+    <message>
+        <location filename="../main_dialog.py" line="186"/>
+        <source>Please add a server</source>
+        <translation>Añadir un servidor por favor</translation>
+    </message>
+    <message>
+        <location filename="../main_dialog.py" line="191"/>
+        <source>Server</source>
+        <translation>Servidores</translation>
+    </message>
+    <message>
+        <location filename="../main_dialog.py" line="317"/>
+        <source>Are you sure you want to remove the server configuration &apos;{selected_server_config}&apos;?</source>
+        <translation>¿Está seguro de que desea eliminar la configuración del servidor &apos;{selected_server_config}&apos;?</translation>
+    </message>
+    <message>
+        <location filename="../main_dialog.py" line="323"/>
+        <source>Success</source>
+        <translation>Éxito</translation>
+    </message>
+    <message>
+        <location filename="../main_dialog.py" line="323"/>
+        <source>Server configuration successfully removed</source>
+        <translation>La configuración del servidor se eliminó correctamente</translation>
+    </message>
+    <message>
+        <location filename="../main_dialog.py" line="364"/>
+        <source>Please complete Mapbender parameters</source>
+        <translation>Por favor complete los parámetros de Mapbender</translation>
+    </message>
+    <message>
+        <location filename="../main_dialog.py" line="364"/>
+        <source>Please enter a valid Mapbender URL title</source>
+        <translation>Ingrese un título de URL de Mapbender válido</translation>
+    </message>
+    <message>
+        <location filename="../main_dialog.py" line="515"/>
+        <source>Success report</source>
+        <translation>Informe de éxito</translation>
+    </message>
+    <message>
+        <location filename="../main_dialog.py" line="442"/>
+        <source>
+                        WMS already existed as a Mapbender source(s) and was successfully reloaded: {name}
+                        &lt;br&gt;&lt;br&gt;
+                        Link to Capabilities:
+                        &lt;br&gt;&lt;br&gt;
+                        &lt;a href=&quot;{wms_url}&quot; style=&quot;color:black;&quot;&gt;{wms_url}&lt;/a&gt;
+                        &lt;br&gt;&lt;br&gt;
+                        Link to Mapbender application:
+                        &lt;br&gt;&lt;br&gt;
+                        &lt;a href=&quot;{link}&quot; style=&quot;color:black;&quot;&gt;{link}&lt;/a&gt;
+                    </source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../main_dialog.py" line="466"/>
+        <source>
+                    WMS successfully created
+                    &lt;br&gt;&lt;br&gt;
+                    Link to Capabilities:
+                    &lt;br&gt;&lt;br&gt;
+                    &lt;a href=&quot;{wms_url}&quot; style=&quot;color:black;&quot;&gt;{wms_url}&lt;/a&gt;
+                    &lt;br&gt;&lt;br&gt;
+                    Link to Mapbender application:
+                    &lt;br&gt;&lt;br&gt;
+                    &lt;a href=&quot;{link}&quot; style=&quot;color:black;&quot;&gt;{link}&lt;/a&gt;
+                    </source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../main_dialog.py" line="530"/>
+        <source>Failed</source>
+        <translation>Fallido</translation>
+    </message>
+    <message>
+        <location filename="../main_dialog.py" line="515"/>
+        <source>
+                    WMS successfully updated in QGIS Server and successfully updated in Mapbender source(s): {name_source}
+                    &lt;br&gt;&lt;br&gt;
+                    Link to Capabilities:
+                    &lt;br&gt;&lt;br&gt;
+                    &lt;a href=&quot;{wms_url}&quot; style=&quot;color:black;&quot;&gt;{wms_url}&lt;/a&gt;
+                    </source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>ServerConfigDialog</name>
+    <message>
+        <location filename="../dialogs/server_config_dialog.py" line="70"/>
+        <source>Save</source>
+        <translation>Ahorrar</translation>
+    </message>
+    <message>
+        <location filename="../dialogs/server_config_dialog.py" line="105"/>
+        <source>Cancel</source>
+        <translation>Cancelar</translation>
+    </message>
+    <message>
+        <location filename="../dialogs/server_config_dialog.py" line="144"/>
+        <source>Test Results</source>
+        <translation>Resultados de la prueda</translation>
+    </message>
+    <message>
+        <location filename="../dialogs/server_config_dialog.py" line="122"/>
+        <source>&lt;b&gt;Failed Tests:&lt;/b&gt;&lt;ul&gt;{errors}&lt;/ul&gt;&lt;b&gt;Successful Tests:&lt;/b&gt;&lt;ul&gt;{successes}&lt;/ul&gt;</source>
+        <translation>Pruebas fallidas:&lt;/b&gt;&lt;ul&gt;{errores}&lt;/ul&gt;&lt;b&gt;Pruebas exitosas:&lt;/b&gt;&lt;ul&gt;{éxitos}&lt;/ul&gt;</translation>
+    </message>
+    <message>
+        <location filename="../dialogs/server_config_dialog.py" line="133"/>
+        <source>&lt;b&gt;Failed Tests:&lt;/b&gt;&lt;ul&gt;{errors}&lt;/ul&gt;</source>
+        <translation>&lt;b&gt;Pruebas fallidas:&lt;/b&gt;&lt;ul&gt;{errores}&lt;/ul&gt;</translation>
+    </message>
+    <message>
+        <location filename="../dialogs/server_config_dialog.py" line="144"/>
+        <source>&lt;b&gt;All tests were successful:&lt;/b&gt;&lt;ul&gt;{successes}&lt;/ul&gt;</source>
+        <translation>&lt;b&gt;Todas las pruebas fueron exitosas:&lt;/b&gt;&lt;ul&gt;{éxitos}&lt;/ul&gt;</translation>
+    </message>
+    <message>
+        <location filename="../dialogs/server_config_dialog.py" line="181"/>
+        <source>Connection to Mapbender was successful.</source>
+        <translation>La conexión a Mapbender fue exitosa.</translation>
+    </message>
+    <message>
+        <location filename="../dialogs/server_config_dialog.py" line="187"/>
+        <source>Token generation failed. Please check your credentials.</source>
+        <translation>Error en la generación del token. Por favor, revise sus credenciales.</translation>
+    </message>
+    <message>
+        <location filename="../dialogs/server_config_dialog.py" line="189"/>
+        <source>Credentials are valid.</source>
+        <comment>Token generation was successful.</comment>
+        <translatorcomment>La generación del token fue exitosa.</translatorcomment>
+        <translation>Las credenciales son válidas.</translation>
+    </message>
+    <message>
+        <location filename="../dialogs/server_config_dialog.py" line="194"/>
+        <source>Server upload is not validated (status code {status_code}: {error_zip_upload}).</source>
+        <translation>La carga del servidor no está validada (código de estado {status_code}: {error_zip_upload}).</translation>
+    </message>
+    <message>
+        <location filename="../dialogs/server_config_dialog.py" line="200"/>
+        <source>Server upload is validated. Upload directory on server: {upload_dir}.</source>
+        <translation>La carga al servidor está validada. Directorio de carga en el servidor: {upload_dir}.</translation>
+    </message>
+    <message>
+        <location filename="../dialogs/server_config_dialog.py" line="204"/>
+        <source>Error</source>
+        <translation>Error</translation>
+    </message>
+    <message>
+        <location filename="../dialogs/server_config_dialog.py" line="204"/>
+        <source>An error occurred during API initialization: {e}.
+API tests (token generation, upload to server, etc.) could not be executed</source>
+        <translation>Se produjo un error durante la inicialización de la API: {e}.
+No se pudieron ejecutar las pruebas de la API (generación de tokens, subida al servidor, etc.)</translation>
+    </message>
+    <message>
+        <location filename="../dialogs/server_config_dialog.py" line="223"/>
+        <source>Unable to connect to the {serverName}. Is the address correct and is the schema supplied (http)? Please see QGIS2Mapbender logs for more information.</source>
+        <translation>No se puede conectar a {serverName}. ¿Es correcta la dirección y se ha proporcionado el esquema (http)? Consulte los registros de QGIS2Mapbender para obtener más información.</translation>
+    </message>
+    <message>
+        <location filename="../dialogs/server_config_dialog.py" line="173"/>
+        <source>Connection to QGIS Server was successful.</source>
+        <translation>La conexión al servidor QGIS fue exitosa.</translation>
+    </message>
+</context>
+<context>
+    <name>mainDialog</name>
+    <message>
+        <location filename="../dialogs/ui/server_config_dialog.ui" line="14"/>
+        <source>QGIS2Mapbender</source>
+        <translation>QGIS2Mapbender</translation>
+    </message>
+    <message>
+        <location filename="../dialogs/ui/main_dialog.ui" line="49"/>
+        <source>Publish / Update QGIS-Project in Mapbender</source>
+        <translation>Publicar / Actualizar proyecto QGIS en Mapbender</translation>
+    </message>
+    <message>
+        <location filename="../dialogs/ui/main_dialog.ui" line="73"/>
+        <source>Mapbender parameters</source>
+        <translation>Parámetros del Mapbender</translation>
+    </message>
+    <message>
+        <location filename="../dialogs/ui/main_dialog.ui" line="80"/>
+        <source>Copy application</source>
+        <translation>Copiar Aplicación</translation>
+    </message>
+    <message>
+        <location filename="../dialogs/ui/main_dialog.ui" line="87"/>
+        <source>Add to an existing application</source>
+        <translation>Agregar a una aplicación existente</translation>
+    </message>
+    <message>
+        <location filename="../dialogs/ui/main_dialog.ui" line="94"/>
+        <source>URL title</source>
+        <translation>Título de la URL</translation>
+    </message>
+    <message>
+        <location filename="../dialogs/ui/main_dialog.ui" line="101"/>
+        <source>Layerset (optional)</source>
+        <translation>Layerset (opcional)</translation>
+    </message>
+    <message>
+        <location filename="../dialogs/ui/main_dialog.ui" line="127"/>
+        <source>Server:</source>
+        <translation>Servidor:</translation>
+    </message>
+    <message>
+        <location filename="../dialogs/ui/main_dialog.ui" line="139"/>
+        <source>If the QGIS project already exists on the server, it will be overwritten</source>
+        <translation>Si el proyecto QGIS ya existe en el servidor, se sobrescribirá</translation>
+    </message>
+    <message>
+        <location filename="../dialogs/ui/main_dialog.ui" line="184"/>
+        <source>Publish </source>
+        <translation type="obsolete">Publicar </translation>
+    </message>
+    <message>
+        <location filename="../dialogs/ui/main_dialog.ui" line="196"/>
+        <source>Update</source>
+        <translation>Acutalizar</translation>
+    </message>
+    <message>
+        <location filename="../dialogs/ui/main_dialog.ui" line="210"/>
+        <source>Update service in Mapbender</source>
+        <translation>Actualizar el servicio en Mapbender</translation>
+    </message>
+    <message>
+        <location filename="../dialogs/ui/main_dialog.ui" line="235"/>
+        <source>Publish in Mapbender application</source>
+        <translation>Publicar la aplicación de Mapbender</translation>
+    </message>
+    <message>
+        <location filename="../dialogs/ui/main_dialog.ui" line="249"/>
+        <source>Server configuration management</source>
+        <translation>Configuración del servidor</translation>
+    </message>
+    <message>
+        <location filename="../dialogs/ui/main_dialog.ui" line="270"/>
+        <source>Servers</source>
+        <translation>Servidores</translation>
+    </message>
+    <message>
+        <location filename="../dialogs/ui/server_config_dialog.ui" line="50"/>
+        <source>Save credentials as plain text          </source>
+        <translation>Guardar credenciales como &apos;plain text&apos;          </translation>
+    </message>
+    <message>
+        <location filename="../dialogs/ui/server_config_dialog.ui" line="57"/>
+        <source>User name</source>
+        <translation>Nombre de usuario</translation>
+    </message>
+    <message>
+        <location filename="../dialogs/ui/server_config_dialog.ui" line="64"/>
+        <source>Password</source>
+        <translation>Contraseña</translation>
+    </message>
+    <message>
+        <location filename="../dialogs/ui/server_config_dialog.ui" line="89"/>
+        <source>Save credentials encrypted in authentication database</source>
+        <translation>Guardar credenciales</translation>
+    </message>
+    <message>
+        <location filename="../dialogs/ui/server_config_dialog.ui" line="101"/>
+        <source>QGIS Servre URL*</source>
+        <translation>URL del servidor QGIS*</translation>
+    </message>
+    <message>
+        <location filename="../dialogs/ui/server_config_dialog.ui" line="118"/>
+        <source>Mapbender base URL*</source>
+        <translation>URL base de Mapbender*</translation>
+    </message>
+    <message>
+        <location filename="../dialogs/ui/server_config_dialog.ui" line="149"/>
+        <source>Server configuration name*                     </source>
+        <translation>Nombre de configuración del servidor*                     </translation>
+    </message>
+    <message>
+        <location filename="../dialogs/ui/server_config_dialog.ui" line="159"/>
+        <source>Authentication</source>
+        <translation>Autenticación</translation>
+    </message>
+    <message>
+        <location filename="../dialogs/ui/server_config_dialog.ui" line="185"/>
+        <source>Test connection</source>
+        <translation>Testear la conexión</translation>
+    </message>
+    <message>
+        <location filename="../dialogs/ui/main_dialog.ui" line="184"/>
+        <source>Publish</source>
+        <translation>Publicar</translation>
+    </message>
+</context>
+</TS>
